8ffa379d464a1b9dcb850da0cc475b5f1bb75221
Maxim Kuzmianok <jst.wrx@gmail.com>
2022-04-28 12:47:41 +0300

    fix: fixed edit input

--- src/Components/TodoList/TodoItem/TodoItem.jsx
+++ src/Components/TodoList/TodoItem/TodoItem.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './TodoItem.module.css';
 
-let TodoItem = ({todo, id, checkComplete, onEditValue}) => {
-    const [ onEdit, setOnEdit ] = useState(false);
-    const [ editValue, setEditValue ] = useState(todo.name);
+let TodoItem = ({ todo, id, checkComplete, onEditValue }) => {
+    const [onEdit, setOnEdit] = useState(false);
+    const [editValue, setEditValue] = useState(todo.name);
 
 
     let onEditButton = () => {
@@ -13,25 +13,26 @@ let TodoItem = ({todo, id, checkComplete, onEditValue}) => {
     let onSaveButton = id => {
         setOnEdit(false)
         if (editValue) {
-            onEditValue( editValue, id )
+            onEditValue(editValue, id)
         } else {
             setEditValue(todo.name)
         }
     }
 
-    if(onEdit) {
+    if (onEdit) {
         return (
             <div>
                 <li className={styles.item_card}>
                     <div>
-                            <input
-                                id={id}
-                                type="text"
-                                value={editValue}
-                                onChange={ e => setEditValue(e.target.value.toLowerCase())}/>
+                        <input
+                            className={styles.card_change}
+                            id={id}
+                            type="text"
+                            value={editValue}
+                            onChange={e => setEditValue(e.target.value.toLowerCase())} />
                     </div>
                     <button
-                        onClick={ () => onSaveButton(id)}
+                        onClick={() => onSaveButton(id)}
                         disabled={editValue === ""}
                         className={styles.item_button}
                         type="button">Save</button>
@@ -50,7 +51,7 @@ let TodoItem = ({todo, id, checkComplete, onEditValue}) => {
                                 value=""
                                 aria-label="..."
                                 checked={todo.complete}
-                                onChange={ () => checkComplete(id)}/>
+                                onChange={() => checkComplete(id)} />
                             <span className={todo.complete ? styles.item_complete : styles.item_body}>{todo.name}</span>
                         </label>
                     </div>
